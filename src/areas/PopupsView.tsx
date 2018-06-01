8e2201be8b4549cc91cb4340fd6d1742645fbdac
tehsolace <tehsolace@gmail.com>
2018-06-01 16:59:06 -0500

    popup progress

--- src/areas/PopupsView.tsx
+++ src/areas/PopupsView.tsx
@@ -1,33 +1,49 @@
 import * as React from 'react';
 
-import {
+import { 
+	Callout,
+	FormGroup,
 	Icon,
-	Popover,
-	PopoverInteractionKind,
-	Position
+	InputGroup
 } from '@blueprintjs/core';
+
+import { Codeable } from 'components/Codeable';
+import { Hint } from 'components/Hint';
 
 export class PopupsView extends React.Component {
 	public render() {
-		const content = (
-			<div className="card codeable-info">
-				<Icon icon="search-text" color="rgb(100, 100, 100)" iconSize={30} />
-				<div className="description">
-					<span>Chronic Depression</span>
-				</div>
-				<div className="coding">
-					<span className="system">ICD-10</span>
-					<span className="code">90211-23</span>
-				</div>
-			</div>
-		);
-
 		return (
 			<>
-				<div className="example stack vertical">
-					<Popover content={content} minimal={true} position={Position.BOTTOM_LEFT} interactionKind={PopoverInteractionKind.CLICK}>
-						Hover over me
-					</Popover>
+				<Callout>
+					Hover over items to display the popups.
+				</Callout>
+				<div className="example">
+					<>Patient is suffering from </>
+					<Codeable description="Chronic Depression" system="ICD-10" code="90233-11" />.
+				</div>
+				<div className="example">
+					<>Patient is suffering from </>
+					<Codeable description="Chronic Depression" system="ICD-10" code="90233-11">
+						depression
+					</Codeable>.
+				</div>
+				<div className="example">
+					<>Patient is suffering from </>
+					<span data-balloon="This is a tip!" data-balloon-pos="down">
+						depression
+					</span>.
+				</div>
+				<div className="example">
+					<>Patient is suffering from depression. </>
+					<span data-balloon="This is a tip!" data-balloon-pos="down">
+						<Icon title="" color="#5d49c8" icon="help" iconSize={14} />
+					</span>
+				</div>
+				<div className="example stack">
+					<FormGroup label="Last Name" labelFor="lastName" requiredLabel={true} 
+						helperText={<>Patient's family name. <Hint text="Input the patient's family name, also known as their sirname or last name." /></>}>
+						<InputGroup id="lastName" />
+					</FormGroup>
 				</div>
 			</>
 		);
