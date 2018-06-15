8aed2cb0f4c1a163b5207fa4eaa724e9fc2bad4d
tehsolace <tehsolae@gmail.com>
2018-06-15 09:08:43 -0500

    random update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,18 @@
 
+import { MissingResourceError } from 'exceptions';
 import * as express from 'express'
 import routes from './routers'
 
 const app = express();
 const apiRouter = express.Router();
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
 routes(apiRouter);
+
+app.use((err: Error, req: express.Request, res: express.Response) => {
+	if (err instanceof MissingResourceError) {
+		res.sendStatus(404);
+	}
+})
 
 export default app;
