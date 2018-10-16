90ea6193464bf64bb80190d0f6d98ad1e086ba7d
Justin Hopper <jhopper@remarkablehealth.com>
2018-10-16 12:07:22 -0500

    Added coloring option for buttons page

--- src/scenes/sandbox/examples/Buttons.tsx
+++ src/scenes/sandbox/examples/Buttons.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {
 	Button,
 	ButtonGroup,
+	Intent,
 	Label,
 	Menu,
 	MenuItem,
@@ -10,50 +11,76 @@ import {
 	Position
 } from '@blueprintjs/core';
 
+import Stack from 'common/components/Stack';
 import Example from '../components/Example';
+import IntentSelect from '../components/IntentSelect';
 
-export default function Buttons() {
-	const moreOptionsMenu = (
-		<Menu>
-			<MenuItem text="Refresh" icon="refresh" />
-			<MenuItem text="Add Comments" icon="annotation" />
-			<MenuItem text="Clear Answers" icon="eraser" />
-		</Menu>
-	);
-
-	return (
-		<>
-			<Example position="middle">
-				<Label>Menu</Label>
-				<Popover content={moreOptionsMenu} position={Position.BOTTOM_LEFT}>
-					<Button icon="menu" minimal={true} />
-				</Popover>
-			</Example>
-			<Example>
-				<ButtonGroup minimal={true}>
-					<Button text="Add Template" icon="add" />
-					<Button text="Load Results" icon="search" />
-					<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
-						<Button text="More Options" icon="cog" rightIcon="caret-down" />
-					</Popover>
-				</ButtonGroup>
-			</Example>
-			<Example>
-				<ButtonGroup>
-					<Button text="Add Template" icon="add" />
-					<Button text="Load Results" icon="search" />
-					<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
-						<Button text="More Options" icon="cog" rightIcon="caret-down" />
-					</Popover>
-				</ButtonGroup>
-			</Example>
-			<Example vertical={false}>
-				<Button text="Add Template" icon="add" />
-				<Button text="Load Results" icon="search" />
-				<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
-					<Button text="More Options" icon="cog" rightIcon="caret-down" />
-				</Popover>
-			</Example>
-		</>
-	);
+export interface IButtonsState {
+	intent: Intent;
 }
+
+export class Buttons extends React.Component<{}, IButtonsState> {
+	constructor(props: {}) {
+		super(props);
+
+		this.state = {
+			intent: Intent.NONE
+		}
+	}
+
+	public render() {
+		const { intent } = this.state;
+
+		const moreOptionsMenu = (
+			<Menu>
+				<MenuItem text="Refresh" icon="refresh" />
+				<MenuItem text="Add Comments" icon="annotation" />
+				<MenuItem text="Clear Answers" icon="eraser" />
+			</Menu>
+		);
+
+		return (
+			<>
+				<Stack position="middle">
+					<Label>Color</Label>
+					<IntentSelect intent={intent} onChange={this.handleIntentChange} />
+				</Stack>
+				<Example position="middle">
+					<Label>Menu</Label>
+					<Popover content={moreOptionsMenu} position={Position.BOTTOM_LEFT}>
+						<Button icon="menu" minimal={true} intent={intent} />
+					</Popover>
+				</Example>
+				<Example>
+					<ButtonGroup minimal={true}>
+						<Button text="Add Template" icon="add" intent={intent} />
+						<Button text="Load Results" icon="search" intent={intent} />
+						<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
+							<Button text="More Options" icon="cog" rightIcon="caret-down" intent={intent} />
+						</Popover>
+					</ButtonGroup>
+				</Example>
+				<Example>
+					<ButtonGroup>
+						<Button text="Add Template" icon="add" intent={intent} />
+						<Button text="Load Results" icon="search" intent={intent} />
+						<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
+							<Button text="More Options" icon="cog" rightIcon="caret-down" intent={intent} />
+						</Popover>
+					</ButtonGroup>
+				</Example>
+				<Example vertical={false}>
+					<Button text="Add Template" icon="add" intent={intent} />
+					<Button text="Load Results" icon="search" intent={intent} />
+					<Popover content={moreOptionsMenu} position={Position.BOTTOM}>
+						<Button text="More Options" icon="cog" rightIcon="caret-down" intent={intent} />
+					</Popover>
+				</Example>
+			</>
+		);
+	}
+
+	private handleIntentChange = (intent: Intent) => this.setState({ intent });
+}
+
+export default Buttons;
